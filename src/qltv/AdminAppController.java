c723d60d299aa7e95616af86cc0b3a112417e8c9
unknown <kimman06@gmail.com>
2019-11-05 14:58:12 +0700

    23th

--- src/qltv/AdminAppController.java
+++ src/qltv/AdminAppController.java
@@ -35,7 +35,6 @@ import javafx.scene.chart.BarChart;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
-import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.DatePicker;
