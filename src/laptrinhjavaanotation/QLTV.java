d806f97eb28a9d3ee439adf5954031b97168fc74
unknown <kimman06@gmail.com>
2019-12-11 00:03:55 +0700

    update database

--- src/laptrinhjavaanotation/QLTV.java
+++ src/laptrinhjavaanotation/QLTV.java
@@ -19,7 +19,7 @@ public class QLTV extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("StaffApp.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
